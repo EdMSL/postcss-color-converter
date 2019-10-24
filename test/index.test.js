765027a68132b734fe3ec48bc27d1b063079d73f
EdMSL <eduard2011rus@gmail.com>
2019-10-24 20:51:29 +0700

    Add mocha and chai, update test file

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,26 @@
-let postcss = require('postcss')
+const postcss = require('postcss')
+const { assert } = require('chai')
 
-let plugin = require('..')
+const plugin = require('../index')
 
-async function run (input, output, opts) {
-  let result = await postcss([plugin(opts)]).process(input, { from: undefined })
-  expect(result.css).toEqual(output)
-  expect(result.warnings()).toHaveLength(0)
-}
+/* eslint-disable prefer-arrow-callback */
+
+describe('postcss-color-converter', function () {
+  function transform (source, opts) {
+    return postcss([plugin(opts)]).process(source).css
+  }
+
+  it('Input color should not be converted', function () {
+    assert.equal(transform(
+      'body: { color: #ffffff; }',
+      {}
+    ), 'body: { color: #ffffff; }')
+  })
+
+  it('Input color must be converted to rgb', function () {
+    assert.equal(transform(
+      'body: { color: #ffffff; }',
+      { outputColorFormat: 'rgb' }
+    ), 'body: { color: rgb(255, 255, 255); }')
+  })
+})
